7e5f11ba16b4bd7a74f3ed6d31eb17bafaa4ed62
Peyman Fazeli <peymanfazeli.work@gmail.com>
2025-11-20 19:56:29 +0330

    fix(packages): fix some postcss packages and render deck

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+// Styles
 import './App.css'
+import Controls from './components/Controls'
+import GameBoard from './components/GameBoard'
 
 function App() {
 
   return (
     <>
+      {/* <h1>Application</h1> */}
+      <div className="p-4">
+        <Controls />
+        <GameBoard />
+      </div>
     </>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,12 @@
-export default function Card() {
-  return (
-    <div>
-      <h1>Card</h1>
-    </div>
-  )
+import type { CardType } from '../features/game/types'
+
+interface CardProps {
+  card: CardType;
+}
+export default function Card({ card }: CardProps) {
+	return (
+		<div className="w-16 h-20 bg-gray-200 rounded flex items-center justify-center text-2xl">
+			{card.face}
+		</div>
+	)
 }
