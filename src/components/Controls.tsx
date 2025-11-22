8b446c40dd817b957fdfe34f84533cb670d19d7c
Peyman Fazeli <peymanfazeli.work@gmail.com>
2025-11-22 19:57:21 +0330

    add(md): add REDUX related comments and md file

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -2,7 +2,9 @@ import { useAppDispatch, useAppSelector } from '../hooks/index'
 import { startGame } from '../features/game/gameSlice'
 
 export default function Controls() {
+  // useAppDispatch is a hook that allows us to dispatch actions to the store
   const dispatch = useAppDispatch();
+  // useAppSelector is a hook that allows us to access the state from the store; Component re-renders automatically when deck changes!
   const deck = useAppSelector((state) => state.game.deck);
   return (
     <div>
